f71d4920628390691c2932ad907fadc3c5159858
maxflorentin <macsee13@gmail.com>
2022-08-26 09:25:38 -0300

    delete resources already exists

--- network.tf
+++ network.tf
@@ -1,10 +1,3 @@
-resource "google_compute_network" "k8s_vpc" {
-  name = "${var.gke_cluster_name}-k8s-vpc"
-
-  # defaults to true.  false = --subnet-mode custom
-  auto_create_subnetworks = "false"
-}
-
 resource "google_compute_subnetwork" "k8s_subnet" {
   name                     = "${var.gke_cluster_name}-subnet"
   ip_cidr_range            = var.primary_ip_cidr
