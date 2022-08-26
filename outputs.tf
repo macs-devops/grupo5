0b254f34da1d33824714077108f9464a382699a3
maxflorentin <macsee13@gmail.com>
2022-08-26 16:45:22 -0300

    delete from outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "connect_to_zonal_cluster" {
 output "enable_workload_identity_on_node_pool" {
   value = "gcloud container node-pools update ${var.gke_nodepool_name} --cluster ${var.gke_cluster_name} --workload-metadata=GKE_METADATA --zone ${var.zone} --project ${var.project_id}"
 }
+
+output "node_pools" {
+  value = data.google_container_cluster.primary.node_pool
+}
