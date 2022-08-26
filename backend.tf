c8f9e2d48004fc19b5ce93ba61369d83fe328ae5
maxflorentin <macsee13@gmail.com>
2022-08-26 11:08:55 -0300

    config backend

--- backend.tf
+++ backend.tf
@@ -1,13 +1,3 @@
-resource "google_storage_bucket" "default" {
-  name          = "pin2022-tfstate"
-  force_destroy = false
-  location      = "US"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
-
 terraform {
  backend "gcs" {
    bucket  = "pin2022-tfstate"
