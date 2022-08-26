84d6fb7597b8ad993ea4bd216b6d0917ef9697a7
maxflorentin <macsee13@gmail.com>
2022-08-26 08:34:35 -0300

    delete variables

--- variables.tf
+++ variables.tf
@@ -1,13 +1,3 @@
-variable "upcloud_user" {
-  description = "Secret provider user"
-  sensitive = true
-}
-
-variable "upcloud_password" {
-  description = "Secret provider password"
-  sensitive = true
-}
-
 variable "project_id" {}
 
 variable "credentials_file_path" {}
