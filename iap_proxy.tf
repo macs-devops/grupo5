b9a6b40d4abdc7c7c87d313b98e12096c596775d
maxflorentin <macsee13@gmail.com>
2022-08-26 16:35:04 -0300

    add data subnets

--- iap_proxy.tf
+++ iap_proxy.tf
@@ -25,27 +25,7 @@ resource "google_compute_firewall" "iap_tcp_forwarding" {
   target_tags   = ["iap"]
 }
 
-
-resource "google_compute_instance" "iap-proxy" {
-  count        = var.enable_private_endpoint ? 1 : 0
-  name         = "gke-iap-proxy"
-  machine_type = "e2-micro"
-  zone         = var.zone
-
-  tags = ["iap"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  # because we're setting a count on the iap_subnet,
-  # we now have to reference it with an index as well
-  network_interface {
-    network    = "projects/pin2022/global/networks/pin-devops-k8s-vpc"
-    subnetwork = "pin-devops-iap-subnet"
-  }
-
-  metadata_startup_script = file("./scripts/startup.sh")
+data "google_compute_instance" "iap-proxy" {
+  name = "gke-iap-prox"
+  zone = var.zone
 }
