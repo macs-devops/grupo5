c7d97c7a04540536bf99707932e2a9424d3b8cd7
maxflorentin <macsee13@gmail.com>
2022-08-29 20:18:38 -0300

    add namespace

--- gke.tf
+++ gke.tf
@@ -2,3 +2,10 @@ data "google_container_cluster" "primary" {
   name     = "pin-devops"
   location = var.regional ? var.region : var.zone
 }
+
+resource "kubernetes_namespace" "service-wordpress" {
+
+ metadata {
+   name = "service-wordpress"
+ }
+}
